70a590cd06118a915527e49cb69f4438f1c6207e
Aariz Rahman <aariz.ice@gmail.com>
2017-11-04 12:57:15 -0700

    Added Project one - Counter

---
+++ 1-Counter/NumberChange/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  NumberChange
+//
+//  Created by Azizur Rahman on 2017-11-04.
+//  Copyright © 2017 Bidween Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var count = 0
+    
+    @IBOutlet var numberLbl: UILabel!
+    @IBOutlet var increase: UIButton!
+    @IBOutlet var decrease: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func increaseAction(_ sender: UIButton) {
+        self.count += 1
+        self.numberLbl.text = "\(self.count)"
+        
+    }
+    
+    @IBAction func decreaseAction(_ sender: UIButton, forEvent event: UIEvent) {
+        self.count -= 1
+        self.numberLbl.text = "\(self.count)"
+        
+    }
+    
+}
+
